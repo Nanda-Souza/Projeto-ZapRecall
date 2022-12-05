6f4caed21daec046d7a24537131d028ac2a4223d
Nanda-Souza <fesouoli@gmail.com>
2022-12-05 07:00:10 -0300

    Conversao para o uso de styled-components

--- src/components/BotoesCard.js
+++ src/components/BotoesCard.js
@@ -2,10 +2,51 @@ import styled from "styled-components"
 
 export default function BotoesCard(){
     return (
-        <div className="container-botoes">
-            <button className="vermelho">Não lembrei</button> 
-            <button className="amarelo">Quase não lembrei</button>
-            <button className="verde">Zap!</button>
-        </div>
+        <ContainerBotoes>
+            <Button className="vermelho">Não lembrei</Button> 
+            <Button className="amarelo">Quase não lembrei</Button>
+            <Button className="verde">Zap!</Button>
+        </ContainerBotoes>
     )
 }
+
+const ContainerBotoes = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-top: 20px;    
+`  
+
+const Button = styled.button`
+    width: 85px;
+    height: 40px;
+    margin-left: 7px;
+    font-family: 'Recursive';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    &.vermelho {
+        color: #FFFFFF;
+        background: #FF3030;
+        border-radius: 5px;
+        border: 1px solid #FF3030;
+      }
+    &.amarelo {
+        color: #FFFFFF;
+        background: #FF922E;
+        border-radius: 5px;
+        border: 1px solid #FF922E;
+      }
+    &.verde {
+        color: #FFFFFF;
+        background: #2FBE34;
+        border-radius: 5px;
+        border: 1px solid #2FBE34;
+      }
+`
+  
+  
+  
--- src/components/ListaCards.js
+++ src/components/ListaCards.js
@@ -4,18 +4,80 @@ import styled from "styled-components"
 export default function ListaCards(){
     return (
         <ul class="lista-perguntas">
-	      <li class="pergunta-aberta">O que é JSX? <img src="assets/seta_virar.png"/></li>
-	      <li class="pergunta-aberta">JSX é uma sintaxe para escrever HTML dentro do JS  
-            <BotoesCard />
-            </li>
-	      <li class="pergunta-fechada">
-          <p class="pergunta">Pergunta 3</p>
-        <img src="assets/seta_play.png"/>
-        </li>
-        <li class="pergunta-fechada">
-          <p class="certo">Pergunta 4</p>
-          <img src="assets/icone_certo.png"/>
-          </li>
+	        <PerguntaAbert>O que é JSX? <img src="assets/seta_virar.png"/></PerguntaAbert>
+	        <PerguntaAbert>JSX é uma sintaxe para escrever HTML dentro do JS  
+                <BotoesCard />
+            </PerguntaAbert>
+	        <PerguntaFechada>
+                <TextoPergunta className="pergunta">Pergunta 3</TextoPergunta>
+                <img src="assets/seta_play.png"/>
+            </PerguntaFechada>
+            <PerguntaFechada>
+                <TextoPergunta className="certo">Pergunta 4</TextoPergunta>
+                <img src="assets/icone_certo.png"/>
+            </PerguntaFechada>
       </ul>
     )
 }
+
+const PerguntaFechada = styled.li`
+    width: 300px;
+    height: 35px;
+    background-color: #FFFFFF;
+    margin: 12px;
+    padding: 15px;
+    box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;    
+`
+const TextoPergunta = styled.p`
+    font-family: 'Recursive';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 19px;
+    &.pergunta {
+        color: #333333;
+    }        
+    &.certo {
+        color: #2FBE34;
+        text-decoration: line-through;
+    }
+    &.erro {
+        color: #FF3030;
+        text-decoration: line-through;
+    }
+    &.quase {
+        color: #FF922E;
+        text-decoration: line-through;
+    }
+`
+const PerguntaAbert = styled.li`
+    width: 300px;
+    margin: 12px;
+    padding: 15px;
+    min-height: 100px;
+    background: #FFFFD5;
+    box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+    border-radius: 5px;
+    font-family: 'Recursive';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 22px;
+    color: #333333;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    img {
+        position: absolute;
+        bottom: 10px;
+        right: 10px;        
+    }
+  
+`
+
+  
